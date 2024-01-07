7a17163d5647e3b0999d7f621c108e53e7463cf9
itscool2b <megagrb16@gmail.com>
2024-01-07 18:13:34 -0500

    cors

--- hanweb/settings.py
+++ hanweb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "",
+    
 ]
 
 ROOT_URLCONF = "hanweb.urls"
