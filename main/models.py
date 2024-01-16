d32e2224bfc372fe4f55d171230c7226299529eb
itscool2b <itscool2@outlook.com>
2024-01-16 13:40:04 -0500

    final

--- main/models.py
+++ main/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class formmodel(models.Model):
-    first_name = models.CharField(max_length=1000)
-    last_name = models.CharField(max_length=1000)
-    email = models.CharField()
-    message = models.TextField()
+    first_name = models.CharField(max_length=1000,null=False)
+    last_name = models.CharField(max_length=1000,null=False)
+    email = models.CharField(max_length=100,null=False)
+    message = models.TextField(max_length=1000,null=False)
     
