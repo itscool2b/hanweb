53d9a88dd516445ee3708e3cf475c1891500fb75
itscool2b <93061248+itscool2b@users.noreply.github.com>
2024-01-22 12:33:21 -0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
+from django.core.mail import EmailMessage
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.core.mail import send_mail
 from .serializers import FormModelSerializer
 from django_ratelimit.decorators import ratelimit
 
@@ -9,27 +9,22 @@ from django_ratelimit.decorators import ratelimit
 def form_submission(request):
     serializer = FormModelSerializer(data=request.data)
     if serializer.is_valid():
-        # Manually validate captcha if required
-        # captcha_response = serializer.validated_data['captcha']
-        # if not validate_captcha(captcha_response):
-        #     return Response({'errors': {'captcha': ['Invalid captcha']}}, status=400)
-
         form_instance = serializer.save()
 
         email_content = f'From: {form_instance.email}\n' \
                         f'Name: {form_instance.first_name} {form_instance.last_name}\n' \
                         f'Message: {form_instance.message}'
 
-        send_mail(
+        email = EmailMessage(
             subject='New Form Submission',
-            message=email_content,
+            body=email_content,
             from_email='hsq0503@gmail.com',  # Use your domain's email address
-            recipient_list=['HanKaiwps@gmail.com'],
-            fail_silently=False,
-            reply_to=[form_instance.email]  # Include the user's email in 'reply-to'
+            to=['HanKaiwps@gmail.com'],  # List of recipients
+            headers={'Reply-To': form_instance.email}  # Include the user's email in 'reply-to'
         )
+
+        email.send(fail_silently=False)
 
         return Response({'status': 'success'})
     else:
-        # Return serializer errors if the data is not valid
         return Response({'errors': serializer.errors}, status=400)
