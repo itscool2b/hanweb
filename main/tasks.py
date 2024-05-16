6a705e3fd98e03934ffc439678b5295fa5b0d14c
itscool2b <93061248+itscool2b@users.noreply.github.com>
2024-05-15 22:23:09 -0400

    Update tasks.py

--- main/tasks.py
+++ main/tasks.py
@@ -3,12 +3,16 @@ from django.core.mail import EmailMessage
 
 @shared_task
 def send_email_task(subject, body, from_email, to, cc):
-    email = EmailMessage(
-        subject=subject,
-        body=body,
-        from_email=from_email,
-        to=to,
-        cc=cc,
-        headers={'Reply-To': cc[0]}
-    )
-    email.send(fail_silently=False)
+    try:
+        email = EmailMessage(
+            subject=subject,
+            body=body,
+            from_email=from_email,
+            to=to,
+            cc=cc,
+            headers={'Reply-To': cc[0]}
+        )
+        email.send(fail_silently=False)
+    except Exception as e:
+        # Log the error
+        print(f"Failed to send email: {e}")
