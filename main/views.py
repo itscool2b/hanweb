d17b43a9187d1fc81a1fdc8a78d47e3c92d2f15d
itscool2b <93061248+itscool2b@users.noreply.github.com>
2024-05-15 21:47:45 -0400

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
-from django.core.mail import EmailMessage
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import FormModelSerializer
 from django_ratelimit.decorators import ratelimit
+from .serializers import FormModelSerializer
+from .tasks import send_email_task
 
 @ratelimit(key='ip', rate='30/m', block=True)
 @api_view(['POST'])
@@ -16,18 +16,14 @@ def form_submission(request):
                         f'Email: {form_instance.email}\n' \
                         f'Message: {form_instance.message}'
 
-        # Create an email message with the Django EmailMessage class
-        email = EmailMessage(
+        # Call the Celery task to send the email
+        send_email_task.delay(
             subject='New Form Submission',
             body=email_content,
-            from_email='megagrb16@gmail.com',  # Your email (the sender)
-            to=['HanKaiWps@gmail.com'],  # The main email address (recipient)
-            cc=[form_instance.email],  # CC to the user's email
-            headers={'Reply-To': form_instance.email}  # Reply-To set to the user's email
+            from_email='megagrb16@gmail.com',
+            to=['HanKaiWps@gmail.com'],
+            cc=[form_instance.email]
         )
-
-        # Send the email
-        email.send(fail_silently=False)
 
         return Response({'status': 'success'})
     else:
